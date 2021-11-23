c9cfc5eb679720482fde6953d2063eb90155eab8
Vahtee Boo <ronnie.kaa@bigmir.net>
2021-11-23 17:32:09 +0200

    Initial Commit

---
+++ CustomLoginView/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CustomLoginView
+//
+//  Created by Vahtee Boo on 23.11.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CustomLoginView/CustomLoginViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomLoginViewApp.swift
+//  CustomLoginView
+//
+//  Created by Vahtee Boo on 23.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct CustomLoginViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
