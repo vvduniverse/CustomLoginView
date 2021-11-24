c2fe316ecf2dd21fb74d9a21f90e0cb374b1ac9b
Vahtee Boo <ronnie.kaa@bigmir.net>
2021-11-24 15:52:29 +0200

    Next steps 2.

--- CustomLoginView/ContentView.swift
+++ CustomLoginView/ContentView.swift
@@ -8,6 +8,8 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var index = 0
     var body: some View {
         
         GeometryReader { _ in
@@ -44,15 +46,196 @@ struct ContentView: View {
                             .clipShape(Circle())
                     }
                     
+                    Button(action: {
+                        
+                    }){
+                        Image("facebook")
+                            .resizable()
+                            .renderingMode(.original)
+                            .frame(width: 50, height: 50)
+                            .clipShape(Circle())
+                    }
+                    
+                    Button(action: {
+                        
+                    }){
+                        Image("twitter")
+                            .resizable()
+                            .renderingMode(.original)
+                            .frame(width: 50, height: 50)
+                            .clipShape(Circle())
+                    }
+                    
                 }
             }
-            
+            .padding(.top, 30)
+            .padding(.vertical)
         }
+        //        .background(Color("Color")).edgesIgnoringSafeArea(.all)
+        .preferredColorScheme(.dark)
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+
+struct CShape: Shape {
+    
+    func path(in rect: CGRect) -> Path {
+        
+        return Path { path in
+            path.move(to: CGPoint(x: rect.width, y: 100))
+            path.addLine(to: CGPoint(x: rect.width, y: rect.height))
+            path.addLine(to: CGPoint(x: 0, y: rect.height))
+            path.addLine(to: CGPoint(x: 0, y: 0))
+        }
+    }
+}
+
+
+struct CShape1: Shape {
+    
+    func path(in rect: CGRect) -> Path {
+        
+        return Path { path in
+            path.move(to: CGPoint(x: 0, y: 100))
+            path.addLine(to: CGPoint(x: 0, y: rect.height))
+            path.addLine(to: CGPoint(x: rect.width, y: rect.height))
+            path.addLine(to: CGPoint(x: rect.width, y: 0))
+        }
+    }
+}
+
+
+struct Login: View {
+    @State private var email = ""
+    @State private var pass = ""
+    @Binding var index: Int
+    
+    var body: some View {
+        ZStack(alignment: .bottom) {
+            VStack {
+                HStack {
+                    VStack(spacing: 10) {
+                        Text("Login")
+                            .foregroundColor(self.index == 0 ? .white : .gray)
+                            .font(.title)
+                            .fontWeight(.bold)
+                        Capsule()
+                            .fill(self.index == 0 ? .blue : .clear)
+                            .frame(width: 100, height: 5)
+                    }
+                    Spacer()
+                }
+                .padding(.top, 30)
+                
+                VStack {
+                    HStack(spacing: 15) {
+                        Image(systemName: "envelop.fill")
+                            .foregroundColor(Color("Color1"))
+                        TextField("Email Address", text: self.$email)
+                    }
+                    Divider()
+                        .background(Color.white.opacity(0.5 ))
+                }
+                .padding(.horizontal)
+                .padding(.top, 40)
+                
+                VStack {
+                    HStack(spacing: 15) {
+                        Image(systemName: "eye.slash.fill")
+                            .foregroundColor(Color("Color1"))
+                        SecureField("Password", text: self.$pass)
+                    }
+                    Divider()
+                        .background(Color.white.opacity(0.5 ))
+                }
+                .padding(.horizontal)
+                .padding(.top, 30)
+                
+                HStack {
+                    Spacer(minLength: 0)
+                    
+                    Button(action: {}) {
+                        Text("Forget Password?")
+                            .foregroundColor(Color.white.opacity(0.6))
+                    }
+                }
+                .padding(.horizontal)
+                .padding(.top, 30)
+            }
+            .padding()
+            .padding(.bottom, 65)
+            .background(Color("Color2"))
+            .clipShape(CShape())
+            .contentShape(CShape())
+            .shadow(color: Color.black.opacity(0.3), radius: 5, x: 0, y: -5)
+            .onTapGesture {
+                self.index = 0
+            }
+            .cornerRadius(35)
+            .padding(.horizontal, 25)
+            
+            
+            Button(action: {}) {
+                Text("LOGIN")
+                    .foregroundColor(.white)
+                    .fontWeight(.bold)
+                    .padding(.vertical)
+                    .padding(.horizontal, 50)
+                    .background(Color("Color1"))
+                    .clipShape(Capsule())
+                    .shadow(color: Color.white.opacity(0.1), radius: 5, x: 0, y: 5)
+            }
+            .offset(y: 35)
+            .opacity(self.index == 0 ? 1 : 0)
+        }
+    }
+}
+
+
+struct SighUp: View {
+    @State private var email = ""
+    @State private var pass = ""
+    @State private var repass = ""
+    @Binding var index: Int
+    
+    var body: some View {
+        ZStack(alignment: .bottom) {
+            VStack {
+                HStack {
+                    Spacer()
+                    
+                    VStack(spacing: 10) {
+                        Text("SighUp")
+                            .foregroundColor(self.index == 1 ? .white : .gray)
+                            .font(.title)
+                            .fontWeight(.bold)
+                        
+                        Capsule()
+                            .fill(self.index == 1 ? Color.blue : Color.clear)
+                            .frame(width: 100, height: 5)
+                    }
+                }
+                .padding(.top, 30)
+                
+                VStack {
+                    
+                    HStack(spacing: 15) {
+                        Image(systemName: "eye.slash.fill")
+                            .foregroundColor(Color("Color1"))
+                        SecureField("Password", text: self.$pass)
+                    }
+                    Divider()
+                        .background(Color.white.opacity(0.5))
+                }
+                .padding(.horizontal)
+                padding(.top, 30)
+            }
+        }
     }
 }
